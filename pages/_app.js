bb809168af57a6fc4078ccd7f50fb81001863881
Umar Yabo Umar <maker.jr@Umars-MacBook-Pro.local>
2022-05-11 07:59:45 +0100

    fix #1: Make website a PWA

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,29 @@
-import '../styles/globals.css';
-import 'tailwindcss/tailwind.css';
-import { Provider } from 'react-redux';
-import { useStore } from '../redux/store';
-import { useEffect } from 'react';
-import jwtDecode from 'jwt-decode';
-import axios from 'axios';
-import Navbar from '../components/layout/Navbar';
-import Script from 'next/script';
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import { Provider } from "react-redux";
+import { useStore } from "../redux/store";
+import { useEffect } from "react";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
+import Navbar from "../components/layout/Navbar";
+import Head from "next/head";
+import Script from "next/script";
 
 // Redux
-import { SET_AUTHENTICATED } from '../redux/types';
-import { logoutUser, getUserData } from '../redux/actions/userActions';
+import { SET_AUTHENTICATED } from "../redux/types";
+import { logoutUser, getUserData } from "../redux/actions/userActions";
 
 // Material UI
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
-import { createTheme } from '@material-ui/core/styles';
-import appTheme from '../util/theme';
-import PropTypes from 'prop-types';
-import withWidth from '@material-ui/core/withWidth';
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
+import appTheme from "../util/theme";
+import PropTypes from "prop-types";
+import withWidth from "@material-ui/core/withWidth";
 
 const theme = createTheme(appTheme);
 
 axios.defaults.baseURL =
-  'https://us-central1-poli-news-77c19.cloudfunctions.net/api';
+  "https://us-central1-poli-news-77c19.cloudfunctions.net/api";
 
 function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
@@ -35,7 +36,7 @@ function App({ Component, pageProps }) {
         store.dispatch(logoutUser());
       } else {
         store.dispatch({ type: SET_AUTHENTICATED });
-        axios.defaults.headers.common['Authorization'] = token;
+        axios.defaults.headers.common["Authorization"] = token;
         store.dispatch(getUserData());
       }
     }
@@ -43,7 +44,13 @@ function App({ Component, pageProps }) {
 
   return (
     <>
-      <Script id='facebook pixel' strategy='afterInteractive'>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
+      <Script id="facebook pixel" strategy="afterInteractive">
         {`!function(f,b,e,v,n,t,s)
                             {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
                                 n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
@@ -73,7 +80,7 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 }
 
 App.propTypes = {
-  width: PropTypes.oneOf(['lg', 'md', 'sm', 'xl', 'xs']).isRequired,
+  width: PropTypes.oneOf(["lg", "md", "sm", "xl", "xs"]).isRequired,
 };
 
 export default withWidth()(App);
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import axios from "axios";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     let post;
-    if (ctx.pathname == '/story/[sid]') {
+    if (ctx.pathname == "/story/[sid]") {
       let res = await axios.get(`post/${ctx.query.sid}`);
       post = res.data;
     }
@@ -15,14 +15,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        {this.props.post ? (
+        {/* {this.props.post ? (
           <Head>
             <meta
               property='og:url'
               content={`https://news.mypoliticalhub.com/story/${this.props.post.id}`}
             />
             <meta property='og:type' content='website' />
-            {/* <meta property="fb:app_id" content="your fb app id" /> */}
+            {/* <meta property="fb:app_id" content="your fb app id" /> 
             <meta property='og:title' content={this.props.post.title} />
             <meta name='twitter:card' content='summary' />
             <meta
@@ -50,21 +50,100 @@ class MyDocument extends Document {
               src='https://www.googletagmanager.com/gtag/js?id=G-H1RJ9WJGML'
             ></script>
           </Head>
-        ) : (
-          <Head>
-            <script
-              async
-              src='https://www.googletagmanager.com/gtag/js?id=G-H1RJ9WJGML'
-            ></script>
-          </Head>
-        )}
+        ) : ( */}
+        <Head>
+          <meta name="application-name" content="MPH News" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="MPH News" />
+          <meta name="description" content="My Political Hub News. Your number 1 stop for Nigerian political news" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#69409e" />
+
+          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/icons/touch-icon-ipad.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="/icons/touch-icon-ipad-retina.png"
+          />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="mask-icon"
+            href="/icons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+
+          <meta name="twitter:card" content="My Political Hub News. The number 1 stop for all Nigerian politics news" />
+          <meta name="twitter:url" content="https://news.mypoliticalhub.com" />
+          <meta name="twitter:title" content="MPH News" />
+          <meta
+            name="twitter:description"
+            content="Nigerian political news at your fingertips"
+          />
+          <meta
+            name="twitter:image"
+            content="https://news.mypoliticalhub.com/icons/icon-192x192.png"
+          />
+          <meta name="twitter:creator" content="@BLK_labs" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="MPH News" />
+          <meta property="og:description" content="Nigerian political news at your fingertips" />
+          <meta property="og:site_name" content="MPH News" />
+          <meta property="og:url" content="https://news.mypoloiticalhub.com" />
+          <meta
+            property="og:image"
+            content="https://news.mypoliticalhub.com/icons/apple-touch-icon.png"
+          />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-H1RJ9WJGML"
+          ></script>
+        </Head>
         <body>
           <noscript>
             <img
-              height='1'
-              width='1'
-              style={{ display: 'none' }}
-              src='https://www.facebook.com/tr?id=847932856093921&ev=PageView&noscript=1'
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=847932856093921&ev=PageView&noscript=1"
             />
           </noscript>
           <Main />
